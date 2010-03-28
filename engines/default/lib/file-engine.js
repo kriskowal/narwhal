ad00aed9a1ed5e0e882ea8c4e02675e309daee0e
kriskowal <kris.kowal@cixar.com>
2010-03-27 17:56:21 -0700

    Added future compatibility shim for engines that provide fs-base but not file-engine.

--- engines/default/lib/file-engine.js
+++ engines/default/lib/file-engine.js
@@ -1,3 +1,33 @@
-throw new Error(
-"This module is no longer supported.  Please contact the\n" + 
-"Narwhal developer team if you see this exception.");
+/*
+require("nawhal/deprecated").deprecated(
+"This engine does not provide file-engine.\n" +
+"The file-engine module is depreacted. Use fs-base");
+*/
+
+var FUTURE = require("file-engine");
+
+exports.FileIO = FUTURE.openRaw;
+exports.move = FUTURE.move;
+exports.remove = FUTURE.remove;
+exports.touch = FUTURE.touch;
+exports.mkdir = FUTURE.makeDirectory;
+exports.rmdir = FUTURE.removeDirectory;
+exports.canonical = FUTURE.canonical;
+exports.cwd = FUTURE.workingDirectory;
+exports.chdir = FUTURE.changeWorkingDirectory;
+exports.owner = FUTURE.owner;
+exports.group = FUTURE.group;
+exports.chgrp = FUTURE.changeGroup;
+exports.permissions = FUTURE.permissions;
+exports.chmod = FUTURE.changePermissions;
+exports.symlink = FUTURE.symbolicLink;
+exports.link = FUTURE.hardLink;
+exports.exists = FUTURE.exists;
+exports.isFile = FUTURE.isFile;
+exports.isDirectory = FUTURE.isDirectory;
+exports.isLink = FUTURE.isLink;
+exports.size = FUTURE.size;
+exports.lastModified = FUTURE.mtime;
+exports.list = FUTURE.list;
+exports.iterate = FUTURE.iterate;
+
