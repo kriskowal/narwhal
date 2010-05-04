d96d9944136a8ea4a9878f01465aae29c34d1db6
User <fastsoft@E1000-trunk-9131+.(none)>
2010-05-03 17:15:23 -0700

    Got working on FreeBSD with Node.  Fixed JSON tool for Node, for the time being.

--- bin/activate.bash
+++ bin/activate.bash
@@ -2,7 +2,7 @@
 if [ -z "${BASH_ARGV[0]}" ]; then
 
     # as a last recourse, use the present working directory
-    PACKAGE_HOME=$(pwd)
+    NARWHAL_PACKAGE_HOME=$(pwd)
 
 else
 
@@ -19,32 +19,32 @@ else
         SELF_PATH=$(cd -- "$DIR" && cd -- $(dirname -- "$SYM") && pwd)/$(basename -- "$SYM")
     done
 
-    PACKAGE_HOME=$(dirname -- "$(dirname -- "$SELF_PATH")")
+    NARWHAL_PACKAGE_HOME=$(dirname -- "$(dirname -- "$SELF_PATH")")
 
 fi
 
-export PACKAGE_HOME
+export NARWHAL_PACKAGE_HOME
 
 # which -s narwhal doesn't work (os x 10.5, kriskowal)
-if [ -f "$PACKAGE_HOME"/bin/narwhal ]; then
-    NARWHAL="$PACKAGE_HOME"/bin/narwhal
-elif [ -f "$PACKAGE_HOME"/packages/narwhal/bin/narwhal ]; then
-    NARWHAL="$PACKAGE_HOME"/packages/narwhal/bin/narwhal
+if [ -f "$NARWHAL_PACKAGE_HOME"/bin/narwhal ]; then
+    NARWHAL="$NARWHAL_PACKAGE_HOME"/bin/narwhal
+elif [ -f "$NARWHAL_PACKAGE_HOME"/packages/narwhal/bin/narwhal ]; then
+    NARWHAL="$NARWHAL_PACKAGE_HOME"/packages/narwhal/bin/narwhal
 else
     env narwhal -e '' >/dev/null 2>&1
     if [ "$?" -ne 127 ]; then
         NARWHAL=narwhal
     else
-        echo "ERROR: narwhal is not in your PATH nor in $PACKAGE_HOME/bin." >&2
+        echo "ERROR: narwhal is not in your PATH nor in $NARWHAL_PACKAGE_HOME/bin." >&2
     fi
 fi
 
-if [ -f "$PACKAGE_HOME"/narwhal.conf ]; then
-    source "$PACKAGE_HOME"/narwhal.conf
+if [ -f "$NARWHAL_PACKAGE_HOME"/narwhal.conf ]; then
+    source "$NARWHAL_PACKAGE_HOME"/narwhal.conf
     export NARWHAL_DEFAULT_ENGINE
 fi
 
 if [ "$NARWHAL" ]; then
-    export PATH="$("$NARWHAL" --package "$PACKAGE_HOME" --path :)"
+    export PATH="$("$NARWHAL" --package "$NARWHAL_PACKAGE_HOME" --path :)"
 fi
 
